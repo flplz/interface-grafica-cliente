c6d40b0e867369c3f3c622ef772c22fafad765e3
Felipe Pacheco <lipe.fagundespacheco@gmail.com>
2023-05-12 10:23:43 -0300

    fix Cliente

--- ClienteController.py
+++ ClienteController.py
@@ -19,12 +19,38 @@ class ClienteController:
             if event == sg.WIN_CLOSED:
                 rodando = False
             elif event == 'Cadastrar':
-                #FIX ME - implementar lógica de cadastro
-                pass
+                nome = values['nome'].strip()
+                codigo = values['codigo']
+                if nome != '' and codigo != '':
+                    resultado = self.adiciona_cliente(codigo,nome)
+                    self.__telaCliente.limpa_lacuna('nome')
+                    self.__telaCliente.limpa_lacuna('codigo')
+                else:
+                    resultado = "Preencha todos os campos"
             elif event == 'Consultar':
-                #FIX ME - implementar lógica de consulta
-                pass
+                nome = values['nome'].strip()
+                codigo = values['codigo']
+                if codigo != '' and nome == '':
+                    resultado = self.busca_codigo(codigo)
+                    self.__telaCliente.limpa_lacuna('codigo')
+                elif codigo == '' and nome != '':
+                    resultado = self.busca_nome(nome)
+                    self.__telaCliente.limpa_lacuna('nome')
+                elif codigo != '' and nome != '':
+                    resultado = "Digite em apenas um campos, por favor"
+                else:
+                    resultado = "Digite em um dos campos, por favor"
             
+            elif event == 'Remover Cadastro':
+                nome = values['nome'].strip()
+                codigo = values['codigo']
+                if codigo != '' and nome == '':
+                    resultado = self.remover_cliente(codigo)
+                    self.__telaCliente.limpa_lacuna('codigo')
+                elif codigo == '':
+                    resultado = "Codigo não informado"
+                else:
+                    resultado = "Para remover o cadastro de um cliente, preencha apenas o campo do codigo"
             if resultado != '':
                 dados = str(resultado)
                 self.__telaCliente.mostra_resultado(dados)
@@ -33,18 +59,45 @@ class ClienteController:
 
 
     def busca_codigo(self, codigo):
-        try:
-            return self.__clientes[codigo]
-        except KeyError:
-            raise KeyError
+        if codigo.isnumeric():
+            codigo = int(codigo)
+            try:
+                return self.__clientes[codigo]
+            except:
+                return "Codigo não encotrado"
+        else:
+            return "Digite apenas numero, por favor"
 
     # cria novo OBJ cliente e adiciona ao dict
     def adiciona_cliente(self, codigo, nome):
-        self.__clientes[codigo] = Cliente(codigo, nome)
+        try:
+            codigo = int(codigo)
+        except:
+            return "Digite apenas numero inteiros, por favor"
+        
+        if codigo not in self.__clientes.keys():
+            self.__clientes[codigo] = Cliente(codigo, nome)
+            return f"{nome}: {codigo} cadastrado com sucesso"
+        else:
+            return "Esse codigo ja esta em uso"
     
-    def busca_nome(self, nome):
-        for key, val in self.__clientes.items():
-            if val.nome == nome:
-                return key 
+    def remover_cliente(self,codigo):
+        try:
+            codigo = int(codigo)
+        except:
+            return "Digite apenas numeros inteiros, por favor"
 
-        raise LookupError
+        if codigo in self.__clientes.keys():
+            del self.__clientes[codigo]
+            return f"Cadastro do cliente de codigo {codigo} removido"
+        else:
+            return "Esse codigo não esta cadastrado a nenhum cliente"
+
+    def busca_nome(self, nome):
+        try:
+            for key, val in self.__clientes.items():
+                if val.nome == nome:
+                    return self.__clientes[key]
+            
+        except:
+            return 'Nome não encontrado'
--- ClienteView.py
+++ ClienteView.py
@@ -8,13 +8,22 @@ class ClienteView():
         self.__window = sg.Window("Consulta de clientes", self.__container ,font=("Helvetica", 14))
 
     def tela_consulta(self):
-        #FIX ME - implementar a GUI e colocar em self.__container
+        sg.theme('Dark Purple 1')
         
-        self.__container = []
+        self.__container = [
+            [sg.Text("Digite o codigo ou o nome do cliente e clique na ação esperada:")],
+            [sg.Text("Nome:", size=(10,1)), sg.InputText('',key='nome')],
+            [sg.Text("Codigo:", size=(10,1)), sg.InputText('',key='codigo')],
+            [sg.Button("Cadastrar"), sg.Button("Consultar"), sg.Button("Remover Cadastro")],
+            [sg.Text('', key='status', size=(50,1))]
+        ]
         self.__window = sg.Window("Consulta de clientes", self.__container ,font=("Helvetica", 14))
 
     def mostra_resultado(self, resultado): 
-        self.__window.Element('resultado').Update(resultado)
+        self.__window.Element('status').Update(resultado)
+
+    def limpa_lacuna(self,key):
+        self.__window.Element(key).Update('')
 
     def le_eventos(self):
         return self.__window.read()
